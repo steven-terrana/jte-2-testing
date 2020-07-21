7bd7ead8b04e0cee822cc35dcdb7ba49c096a9ce
steven-terrana <steventerrana@gmail.com>
2020-07-21 19:44:55 -0400

    Initial commit

---
+++ src/main/groovy/MyLibraries.groovy
@@ -0,0 +1,17 @@
+/*
+    the combination of package/class name will need to be 
+    unique per jenkins instance in the case where you
+    plan to install multiple library providing plugins 
+*/
+package customize_this
+
+
+import org.boozallen.plugins.jte.config.libraries.LibraryProvidingPlugin
+import org.boozallen.plugins.jte.config.libraries.LibraryProvidingPlugin.LibraryProvidingPluginDescriptor
+import hudson.Extension 
+import org.kohsuke.stapler.DataBoundConstructor 
+
+class MyLibraries extends LibraryProvidingPlugin{
+    @DataBoundConstructor MyLibraries(){}
+    @Extension public static class DescriptorImpl extends LibraryProvidingPluginDescriptor{}
+}
